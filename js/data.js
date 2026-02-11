93cfd38cbf5441d346f07652f710d49fdb60b0e8
Ash Reyes <asrmvrck@gmail.com>
2026-02-11 22:44:57 +0800

    mock data seeding

--- js/data.js
+++ js/data.js
@@ -35,3 +35,112 @@ const technicianCredentials = {
     "paul_placer@dlsu.edu.ph": "notyourpassword",
     "ashley_reyes@dlsu.edu.ph": "hungryaf1234"
 }
+
+const labData = [
+  {
+    id: 1,
+    building: "Gokongwei",
+    room: "GK303",
+    capacity: 30,
+    open: true
+  },
+  {
+    id: 2,
+    building: "Velasco",
+    room: "VL204",
+    capacity: 25,
+    open: true
+  },
+  {
+    id: 3,
+    building: "Miguel",
+    room: "MH101",
+    capacity: 20,
+    open: false
+  }
+];
+
+const reservationData = [
+  {
+    id: 1,
+    name: "Nico Yazawa",
+    email: "nico_yazawa@dlsu.edu.ph",
+    labId: 1,
+    date: "2026-02-12",
+    time: "9:00 AM",
+    seats: 2,
+    companions: "Maki Nishikino"
+  },
+
+  {
+    id: 2,
+    name: "Maki Nishikino",
+    email: "maki_nishikino@dlsu.edu.ph",
+    labId: 1,
+    date: "2026-02-12",
+    time: "9:00 AM",
+    seats: 3,
+    companions: ""
+  },
+
+  {
+    id: 3,
+    name: "Char Aznable",
+    email: "char_aznable@dlsu.edu.ph",
+    labId: 2,
+    date: "2026-02-13",
+    time: "1:30 PM",
+    seats: 1,
+    companions: ""
+  },
+
+  {
+    id: 4,
+    name: "Nico Yazawa",
+    email: "nico_yazawa@dlsu.edu.ph",
+    labId: 2,
+    date: "2026-02-13",
+    time: "2:00 PM",
+    seats: 4,
+    companions: "The Best School Idols"
+  },
+
+  {
+    id: 5,
+    name: "Maki Nishikino",
+    email: "maki_nishikino@dlsu.edu.ph",
+    labId: 1,
+    date: "2026-02-14",
+    time: "10:30 AM",
+    seats: 2,
+    companions: ""
+  },
+
+  {
+    id: 6,
+    name: "Char Aznable",
+    email: "char_aznable@dlsu.edu.ph",
+    labId: 1,
+    date: "2026-02-14",
+    time: "10:30 AM",
+    seats: 5,
+    companions: "Some Guy"
+  }
+];
+
+if(!localStorage.getItem("users")){
+    localStorage.setItem("users", JSON.stringify(userData));
+}
+
+if (!localStorage.getItem("labs")) {
+  localStorage.setItem("labs", JSON.stringify(labData));
+}
+
+if (!localStoragegetItem("reservations")) {
+  localStorage.setItem("reservations", JSON.stringify(reservationData));
+}
+if (!localStorage.getItem("users")) {
+  localStorage.setItem("users", JSON.stringify(userData));
+}
+
+
