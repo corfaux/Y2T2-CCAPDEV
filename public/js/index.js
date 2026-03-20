0f93d0c283ceca2ea10403bbbd753375b0695f90
givemeanameidk <paul_placer@dlsu.edu.ph>
2026-03-20 20:00:13 +0800

    feat: implement admin login, remove mode switch

--- public/js/index.js
+++ public/js/index.js
@@ -4,16 +4,16 @@ async function login(event) {
   const email = document.getElementById("email-field").value;
   const password = document.getElementById("password-field").value;
 
-  if (!userLoginMode) {
-    // Technician login remains the same
-    if(email in technicianCredentials && password === technicianCredentials[email]) {
-      sessionStorage.setItem("currentTechnician", email);
-      window.location.href = "lab-management.html";
-    } else {
-      alert("Incorrect technician credentials");
-    }
-    return;
-  }
+  // if (!userLoginMode) {
+  //   // Technician login remains the same
+  //   if(email in technicianCredentials && password === technicianCredentials[email]) {
+  //     sessionStorage.setItem("currentTechnician", email);
+  //     window.location.href = "lab-management.html";
+  //   } else {
+  //     alert("Incorrect technician credentials");
+  //   }
+  //   return;
+  // }
 
   // student login: call backend
   try {
@@ -30,38 +30,13 @@ async function login(event) {
       return;
     }
 
-    // save the full MongoDB object with _id
     sessionStorage.setItem("currentUser", JSON.stringify(data));
-
-    // redirect to student profile
-    window.location.href = "student-profile.html";
+    window.location.href = data.role === "student" ? "student-profile.html" : "lab-management.html";
   } catch (err) {
     console.error("Login error:", err);
     alert("Server error. Try again later.");
   }
 }
 
-function changeLoginMode(event) {
-    const welcomeMsgHeader = document.querySelector(".welcome-message h2");
-    const welcomeMsgPara = document.querySelector(".welcome-message p");
 
-    if(userLoginMode) {
-        welcomeMsgHeader.textContent = "Lab Technician Login";
-        welcomeMsgPara.textContent = "Log in to manage users, reservations, and laboratories.";
-        switchModeBtn.textContent = "User Login";
-        userLoginMode = false;
-    } else {
-        welcomeMsgHeader.textContent = "Welcome, Lasallian!";
-        welcomeMsgPara.textContent = "Use your DLSU email address to continue with LabSys!";
-        switchModeBtn.textContent = "Lab Technician Login";
-        userLoginMode = true;
-    }
-}
-
-
-let userLoginMode = true;
-const loginForm = document.querySelector(".login-pane form");
-const switchModeBtn = document.getElementById("switch-mode-btn");
-
-loginForm.addEventListener("submit", login);
-switchModeBtn.addEventListener("click", changeLoginMode);
+document.querySelector(".login-pane form").addEventListener("submit", login);
