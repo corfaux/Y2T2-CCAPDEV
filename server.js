82f61df84952a2236dbe23a69112b499949f8c54
Ash Reyes <asrmvrck@gmail.com>
2026-03-21 19:20:43 +0800

    fix: fixed directory routing

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload({ createParentPath: true }));
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // --- Routes ---
 const accountRoutes = require("./src/routes/AccountRoutes");
