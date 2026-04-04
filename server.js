bb7119e5e26b7387106a55e8267ce9ff32d1cedf
Ash Reyes <asrmvrck@gmail.com>
2026-04-04 16:59:49 +0800

    fix: added CORS origins for render.io

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const allowedOrigins = [
   "http://localhost:5500",
   "http://127.0.0.1:5000",
   "http://localhost:5000",
-  "http://localhost:3000"
+  "http://localhost:3000",
+  "https://labsys-d4fk.onrender.com"
 ];
 
 app.use(cors({
